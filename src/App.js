af654654eea963580068dda5beda8f2fefb19432
HwangTaeyoung1999 <97142307+HwangTaeyoung1999@users.noreply.github.com>
2022-06-14 03:21:10 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ReactDOM } from 'react';
+import background from"./img/stonkperson.jpg"; 
+
+
 
 function App() {
   return (
-    <div className="App">
-      <div className= "search-container">
-
+  <div className="Background">
+    <img src={background} alt="stonk"/>
+    <div className="Search">
+      <div className="input">
+        <input type="text" placeholder="Search.."/>
+               
       </div>
     </div>
+  </div>
   );
 }
 
