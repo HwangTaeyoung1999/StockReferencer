478e9d96638ef6299b2c4a45e6536f82811cd6f0
HwangTaeyoung1999 <97142307+HwangTaeyoung1999@users.noreply.github.com>
2022-07-29 02:11:49 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const axios = require('axios');
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
+// get driver connection
+const dbo = require("./db/conn");
+ 
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+ 
+  });
+  console.log(`Server is running on port: ${port}`);
+});
+
+//Serving static files
+app.use(express.static('public'));
+
+//Calling the News api
+
+const res = async () => {
+  try {
+    return await axios.get('https://newsapi.org/v2/everything?q=Apple&from=2022-07-29&sortBy=popularity&apiKey=2de0f9b05b0b49f990cc46f7dd0546b6')
+  } catch (error) {
+    console.error(error)
+  }
+}
+
