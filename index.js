f92365bd09347d1ac36990c315f5ea1fce8205a9
Taeyoung Hwang <97142307+HwangTaeyoung1999@users.noreply.github.com>
2022-12-28 19:48:21 -0500

    Add files via upload
    External api calls

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const axios = require('axios');
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
-app.use(cors());
-app.use(express.json());
-app.use(require("./routes/record"));
-// get driver connection
-const dbo = require("./db/conn");
- 
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
-  });
-  console.log(`Server is running on port: ${port}`);
+const express = require('express')
+const app = express()
+const request = require('request')
+
+
+const url = new URL('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=?&interval=5min&apikey=636AGY53C52H8XCC');
+const search_params = url.searchParams;
+search_params.set('symbol','TSLA');
+
+url.search = search_params.toString();
+const new_url = url.toString();
+console.log(new_url);
+
+
+request.get({
+    url: url,
+    json: true,
+    headers: {'User-Agent': 'request'}
+  }, (err, res, data) => {
+    if (err) {
+      console.log('Error:', err);
+    } else if (res.statusCode !== 200) {
+      console.log('Status:', res.statusCode);
+    } else {
+      // data is successfully parsed as a JSON object:
+      console.log(data);
+    }
 });
-
-//Serving static files
-app.use(express.static('public'));
-
-//Calling the News api with axios
-axios.get('https://newsapi.org/v2/everything?q=stock&apiKey=2de0f9b05b0b49f990cc46f7dd0546b6')
-  .then(result => {
-      console.log(result.data);
-  })
-  .catch(error => {
-      console.log(error);
-  });
 
 
 
